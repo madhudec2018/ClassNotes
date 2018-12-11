8de9d70190ca0c1ea92b7d9eb2a8b2ed3ae3f2b2
Madhu <madhu.dec2018.1@yandex.com>
2018-12-10 21:06:24 -0600

    added Class 8 files

---
+++ Class8/programstoCover.js
@@ -0,0 +1,51 @@
+/* 
+
+In Class
+1)
+Given a number N, print sum of all even numbers from 1 to N.
+Input Format :
+
+Integer N
+
+Output Format :
+
+Required Sum 
+
+Sample Input 1 :
+
+ 6
+
+Sample Output 1 :
+
+12
+
+
+2) 
+ Print the following pattern
+Pattern for N = 4
+
+1
+23
+345
+4567
+
+Input Format :
+
+N (Total no. of rows)
+
+Output Format :
+
+Pattern in N lines
+
+Sample Input 1 :
+
+3
+
+Sample Output 1 :
+
+1
+23
+345
+
+
+ */
